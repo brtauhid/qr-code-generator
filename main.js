556177cefb0669d033d7230f539b1440c84d8b3d
brtauhid <br.tauhid@gmail.com>
2024-04-25 11:57:38 +0600

    html structure and css

---
+++ main.js
@@ -0,0 +1,8 @@
+const imgBox = document.getElementById('imgBox');
+const qrImg = document.getElementById('qrImage');
+const qrText = document.getElementById('qrText');
+
+
+function generateQR(){
+    qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+ qrText.value;
+}
